b488a82596d651de5edf4f8207b7a6801ca44f26
I Gusti Agung Gede Darma Putra <igustiagunggededarmaputra28@gmail.com>
2025-07-01 11:46:20 +0800

    Create Auth Service Added generateResetPasswordLink, resetPassword, register, login, refreshAccessToken to Auth Service

---
+++ app/Service/AuthService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\URL;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class AuthService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function generateResetPasswordLink($data){
+        return Url::temporarySignedRoute(
+            'generate-reset-password', now()->addMinutes(30), ['user' => $data['user_id']]
+        );
+    }
+
+    public function resetPassword($data){
+        $user = User::findOrFail($data['user_id']);
+        $user->update(['password'=>Hash::make($data['password'])]);
+        return [
+            "user"=>$user
+        ];
+    }
+
+    public function register($data)
+    {
+        $user = User::create($data);
+        return [
+            "user" => $user,
+            "accessToken" => $user->generatePlainTextAccessToken(),
+            "refreshToken" => $user->generatePlainTextRefreshToken()
+        ];
+    }
+
+    public function login($data)
+    {
+        $user = User::where('email', $data['email'])->firstOrFail();
+        if (!Hash::check($data['password'], $user->password)) {
+            abort(401);
+        }
+        $user->tokens()->delete();
+        return [
+            "user" => $user,
+            "accessToken" => $user->generatePlainTextAccessToken(),
+            "refreshToken" => $user->generatePlainTextRefreshToken()
+        ];
+    }
+
+
+    public function refreshAccessToken(String $currentRefreshToken)
+    {
+        $refreshToken = PersonalAccessToken::findToken($currentRefreshToken);
+        if (!$refreshToken || $refreshToken->name !== "refreshToken" || !$refreshToken->can('auth:refresh') || !$refreshToken->expires_at || $refreshToken->expires_at->isPast()) {
+            abort(401);
+        }
+        $user = $refreshToken->tokenable;
+        /**
+         * @var User $user
+         */
+        $user->tokens()->delete();
+        return [
+            "user" => $user,
+            "accessToken" => $user->generatePlainTextAccessToken(),
+            "refreshToken" => $user->generatePlainTextRefreshToken()
+        ];
+    }
+}
