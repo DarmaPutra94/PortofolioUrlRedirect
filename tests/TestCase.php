fc5d7b205702e6c794913e8b1d5be2f92620f5cb
I Gusti Agung Gede Darma Putra <igustiagunggededarmaputra28@gmail.com>
2025-07-07 13:13:02 +0800

    +Added protected function generate_test_user to create and login a test generate_test_user +Added protected function generate_test_url_short to create short url for given user

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use App\Models\UrlShort;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function generate_test_user(): mixed
+    {
+        $user = User::factory()->create(['password'=>"Test123"]);
+        $response = $this->postJson(route('auth.login'), [
+            "email" => $user->email,
+            "password" => "Test123"
+        ]);
+        $json = (object) $response->json();
+        return $json;
+    }
+
+    protected function generate_test_url_short(User $user): mixed{
+        $short_url = UrlShort::factory()->for($user)->create();
+        return $short_url;
+    }
 }
