dd6eb751b2899d71e126026d7a8500da771d27a6
I Gusti Agung Gede Darma Putra <igustiagunggededarmaputra28@gmail.com>
2025-07-07 13:07:21 +0800

    +Fix short_code being called short_url -Remove url_hash

--- app/Models/UrlShort.php
+++ app/Models/UrlShort.php
@@ -2,15 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UrlShort extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'url',
-        'url_hash',
-        'short_url',
+        'short_code',
         'access_count'
     ];
 
