07761ee03ae41109a1fe985b5aa4f7739a41b190
I Gusti Agung Gede Darma Putra <igustiagunggededarmaputra28@gmail.com>
2025-07-07 13:10:24 +0800

    +Modify most of the function to return user instead of formatted array +Modify so that abort format will be json friendly

--- app/Service/AuthService.php
+++ app/Service/AuthService.php
@@ -17,42 +17,40 @@ class AuthService
         //
     }
 
-    public function generateResetPasswordLink($data){
+    public function generateResetPasswordLink($data)
+    {
         return Url::temporarySignedRoute(
-            'generate-reset-password', now()->addMinutes(30), ['user' => $data['user_id']]
+            'generate-reset-password',
+            now()->addMinutes(30),
+            ['user' => $data['user_id']]
         );
     }
 
-    public function resetPassword($data){
+    public function resetPassword($data)
+    {
         $user = User::findOrFail($data['user_id']);
-        $user->update(['password'=>$data['password']]);
+        $user->update(['password' => $data['password']]);
         return [
-            "user"=>$user
+            "user" => $user
         ];
     }
 
     public function register($data)
     {
         $user = User::create($data);
-        return [
-            "user" => $user,
-            "accessToken" => $user->generatePlainTextAccessToken(),
-            "refreshToken" => $user->generatePlainTextRefreshToken()
-        ];
+        return $user;
     }
 
     public function login($data)
     {
         $user = User::where('email', $data['email'])->firstOrFail();
         if (!Hash::check($data['password'], $user->password)) {
-            abort(401);
+            abort(response()->json([
+                'message' => 'Wrong credential',
+            ], 401));
         }
         $user->tokens()->delete();
-        return [
-            "user" => $user,
-            "accessToken" => $user->generatePlainTextAccessToken(),
-            "refreshToken" => $user->generatePlainTextRefreshToken()
-        ];
+        return $user;
     }
 
 
@@ -60,17 +58,15 @@ class AuthService
     {
         $refreshToken = PersonalAccessToken::findToken($currentRefreshToken);
         if (!$refreshToken || $refreshToken->name !== "refreshToken" || !$refreshToken->can('auth:refresh') || !$refreshToken->expires_at || $refreshToken->expires_at->isPast()) {
-            abort(401);
+            abort(response()->json([
+                'message' => 'Refresh token is expired',
+            ], 401));
         }
         $user = $refreshToken->tokenable;
         /**
          * @var User $user
          */
         $user->tokens()->delete();
-        return [
-            "user" => $user,
-            "accessToken" => $user->generatePlainTextAccessToken(),
-            "refreshToken" => $user->generatePlainTextRefreshToken()
-        ];
+        return $user;
     }
 }
