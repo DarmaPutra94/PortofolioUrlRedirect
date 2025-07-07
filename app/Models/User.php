4e1c4bcdfb8f501612a13cf41e49384e71849055
I Gusti Agung Gede Darma Putra <igustiagunggededarmaputra28@gmail.com>
2025-07-07 13:08:25 +0800

    +Make it so user model use HasApiToken for Sanctum +Added generateAccessToken, generatePlainTextAccessToken, generateRefreshToken, generatePlainTextRefreshToken to handle access and refresh token creation.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -46,6 +49,27 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function generateAccessToken()
+    {
+        return $this->createToken('accessToken', ['*'], Carbon::now()->addHour());
+    }
+
+    public function generatePlainTextAccessToken()
+    {
+        return $this->generateAccessToken()->plainTextToken;
+    }
+
+    public function generateRefreshToken()
+    {
+        return $this->createToken('refreshToken', ['auth:refresh'], Carbon::now()->addDays(7));
+    }
+
+    public function generatePlainTextRefreshToken()
+    {
+        return $this->generateRefreshToken()->plainTextToken;
+    }
+
 
     public function shortUrls() : HasMany{
         return $this->hasMany(UrlShort::class);
