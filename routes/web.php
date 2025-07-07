c7864548ab8334426548a4e02912527eeadb04b0
I Gusti Agung Gede Darma Putra <igustiagunggededarmaputra28@gmail.com>
2025-07-07 13:07:00 +0800

    Added route to handle redirect

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\UrlShortController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{short_code}', [UrlShortController::class, 'redirect']);
