909d2ee6336d5bde2e0a154e499cab489c8265a5
I Gusti Agung Gede Darma Putra <igustiagunggededarmaputra28@gmail.com>
2025-07-20 10:26:26 +0800

    +Added /Api/ to match new split controller to api.php +Added all route to handle frontend request to web.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UrlShortController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UrlShortController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,18 +9,19 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
 Route::prefix('auth')->name("auth.")->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
-    Route::middleware('auth:sanctum')->group(function(){
+    Route::middleware('auth:sanctum')->group(function () {
         Route::post("/logout", [AuthController::class, "logout"])->name('logout');
     });
 });
 
-Route::prefix('shorturl')->name("shorturl.")->middleware('auth:sanctum')->group(function(){
+Route::prefix('shorturl')->name("shorturl.")->middleware('auth:sanctum')->group(function () {
     Route::post('/store', [UrlShortController::class, "store"])->name('store');
-    Route::prefix('{short_code}')->group(function(){
+    Route::prefix('{short_code}')->group(function () {
         Route::get('', [UrlShortController::class, 'show'])->name('show');
         Route::put('', [UrlShortController::class, "update"])->name('update');
         Route::delete('', [UrlShortController::class, "destroy"])->name('destroy');
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,30 @@
 <?php
 
-use App\Http\Controllers\UrlShortController;
+use App\Http\Controllers\Web\AuthController;
+use App\Http\Controllers\Api\UrlShortController as UrlShortApiController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\UrlShortController;
 
-Route::get('/{short_code}', [UrlShortController::class, 'redirect'])->name('shorturl.redirect');
+Route::name('frontend.')->group(function () {
+    Route::middleware(['guest'])->group(function () {
+        Route::get('login', [AuthController::class, 'loginView'])->name('view-login');
+        Route::post('login', [AuthController::class, 'login'])->name('login');
+        Route::get('register', [AuthController::class, 'registerView'])->name('view-register');
+        Route::post('register', [AuthController::class, 'register'])->name('register');
+        Route::get('request-reset-password', [AuthController::class, 'requestResetPasswordView'])->name('view-request-reset-password');
+        Route::post('request-reset-password', [AuthController::class, 'requestResetPassword'])->name('request-reset-password');
+        Route::get('reset-password/{token}/{user_id}', [AuthController::class, 'resetPasswordView'])->name('view-reset-password');
+        Route::post('reset-password/{token}/{user_id}', [AuthController::class, 'resetPassword'])->name('reset-password');
+    });
+
+    Route::middleware(['auth'])->group(function () {
+        Route::get('', [UrlShortController::class, 'create'])->name('create');
+        Route::put('{short_code}', [UrlShortApiController::class, "update"])->name('update');
+        Route::get('dashboard', [UrlShortController::class, 'dashboard'])->name('dashboard');
+        Route::delete('{short_code}', [UrlShortApiController::class, "destroy"])->name('destroy');
+        Route::post('shorturl', [UrlShortController::class, 'store'])->name('store');
+        Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+    });
+});
+
+Route::get('/{short_code}', [UrlShortApiController::class, 'redirect'])->name('shorturl.redirect');
